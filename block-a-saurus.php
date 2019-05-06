a61321e1b706324d226a1e244faecc0e87724d23
Keanan Koppenhaver <keanan@alphaparticle.com>
2019-05-06 18:38:05 -0500

    Get basic block with icon in editor

--- block-a-saurus.php
+++ block-a-saurus.php
@@ -14,3 +14,48 @@
  * License:     GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
+
+add_action( 'enqueue_block_editor_assets', 'bas_editor_assets' );
+add_action( 'enqueue_block_assets', 'bas_frontend_assets' );
+
+/**
+ * Enqueue the block's assets for the editor.
+ *
+ * `wp-blocks`: includes block type registration and related functions.
+ * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
+ * `wp-i18n`: To internationalize the block's text.
+ *
+ * @since 1.0.0
+ */
+function bas_editor_assets() {
+	// Scripts.
+	wp_enqueue_script(
+		'bas-basic-script', // Handle.
+		plugins_url( 'block.build.js', __FILE__ ),
+		array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'block.js' )
+	);
+
+	// Styles.
+	wp_enqueue_style(
+		'bas-basic-styles', // Handle.
+		plugins_url( 'editor.css', __FILE__ ),
+		array( 'wp-edit-blocks' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' )
+	);
+}
+
+/**
+ * Enqueue the block's assets for the frontend.
+ *
+ * @since 1.0.0
+ */
+function bas_frontend_assets() {
+	// Styles.
+	wp_enqueue_style(
+		'bas-basic-frontend', // Handle.
+		plugins_url( 'style.css', __FILE__ ),
+		array( 'wp-blocks' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' )
+	);
+}
