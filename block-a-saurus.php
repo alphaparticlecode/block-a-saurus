f27308c74f12103beffc1dc6977446b292e46ca3
Keanan Koppenhaver <keanan@alphaparticle.com>
2019-05-06 17:23:01 -0500

    init commit

---
+++ block-a-saurus.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin's bootstrap file to launch the plugin.
+ *
+ * @wordpress-plugin
+ * Plugin Name: Block-a-saurus
+ * Plugin URI:  https://alphaparticle.com/blockasaurus
+ * Description: A Gutenberg block that lets users play the T-rex game from Chrome right within a post!
+ * Version:     1.0
+ * Author:      Alpha Particle
+ * Author URI:  https://alphaparticle.com/
+ * Text Domain: blockasaurus
+ * Domain Path: /languages
+ * License:     GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
